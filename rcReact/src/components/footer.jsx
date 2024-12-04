6138c04585908581f10b915f096b2906f95c1f67
yeeeeefang <sweetcat881314@gmail.com>
2024-12-04 19:11:34 +0800

    1204updatefooter-2

--- rcReact/src/components/footer.jsx
+++ rcReact/src/components/footer.jsx
@@ -2,24 +2,32 @@ function Footer() {
     return (
         <div>
             <nav className="nav1">
-                <ul className="item1"><p>首頁</p>
-                <h2>首頁</h2>
-                    <li className="item2">關於我們</li>
-                    
-                </ul>
-                <ul className="item1"><p>學習專區</p>
-                    <li className="item2">學習區</li>
-                    <li className="item2">小試身手</li>
-                    <li className="item2">探索學習趨勢</li>
-                    
-                </ul>
-                <ul className="item1"><p>會員專區</p>
-                    <li className="item2">登入/註冊</li>
-                    <li className="item2">我的帳號</li>
-                    
-                </ul>
-            </nav>
-        </div>
+                <div id="nav1-1">
+                    <ul className="item1"><p>首頁</p>
+                        <li className="item2">關於我們</li>
+
+                    </ul>
+                    <ul className="item1"><p>學習專區</p>
+                        <li className="item2">學習區</li>
+                        <li className="item2">小試身手</li>
+                        <li className="item2">探索學習趨勢</li>
+
+                    </ul>
+                    <ul className="item1"><p>會員專區</p>
+                        <li className="item2">登入/註冊</li>
+                        <li className="item2">我的帳號</li>
+
+                    </ul>
+                </div>
+                <div id="nav1-2">
+                    <img src="" alt="" />
+                    <ul class="icon">
+                        <li><a class="icon-fb" href=""></a></li>
+        
+                    </ul>
+                </div>
+            </nav >
+        </div >
     )
 }
 export default Footer
