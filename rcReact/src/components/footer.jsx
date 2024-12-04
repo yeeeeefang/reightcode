c4f8f6f94b91818408812fb087b3364f7b7df0d6
yeeeeefang <sweetcat881314@gmail.com>
2024-12-04 18:55:36 +0800

    1204updatescss and upfooter

--- rcReact/src/components/footer.jsx
+++ rcReact/src/components/footer.jsx
@@ -1,14 +1,25 @@
-function footer() {
+function Footer() {
     return (
         <div>
-            <nav id="nav1">
-                <ul>
-                    <li className="item">首頁</li>
-                    <li className="item">學習專區</li>
-                    <li className="item">會員專區</li>
+            <nav className="nav1">
+                <ul className="item1"><p>首頁</p>
+                <h2>首頁</h2>
+                    <li className="item2">關於我們</li>
+                    
+                </ul>
+                <ul className="item1"><p>學習專區</p>
+                    <li className="item2">學習區</li>
+                    <li className="item2">小試身手</li>
+                    <li className="item2">探索學習趨勢</li>
+                    
+                </ul>
+                <ul className="item1"><p>會員專區</p>
+                    <li className="item2">登入/註冊</li>
+                    <li className="item2">我的帳號</li>
+                    
                 </ul>
             </nav>
         </div>
     )
 }
-export default footer
+export default Footer
