43432066c217c379c13f4e46e866541bdf42cb40
yeeeeefang <sweetcat881314@gmail.com>
2024-12-04 13:40:35 +0800

    1204 add.scss

---
+++ rcReact/src/components/Card.jsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+
+const Card = (props) => {
+    return (
+        <div>
+         
+          <div className="box">
+           <h3>{props}</h3>
+           <p>{props}</p>
+          
+           <a href="" className="btn">Buy now</a>
+         </div>
+            
+        </div>
+    )
+}
+
+export default Card
