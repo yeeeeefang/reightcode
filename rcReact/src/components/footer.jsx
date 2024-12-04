fdedc06bd370ca306023a1a2c1719dcc97d6b97c
yeeeeefang <sweetcat881314@gmail.com>
2024-12-04 18:18:45 +0800

    1204foot

---
+++ rcReact/src/components/footer.jsx
@@ -0,0 +1,14 @@
+function footer() {
+    return (
+        <div>
+            <nav id="nav1">
+                <ul>
+                    <li className="item">首頁</li>
+                    <li className="item">學習專區</li>
+                    <li className="item">會員專區</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+export default footer
