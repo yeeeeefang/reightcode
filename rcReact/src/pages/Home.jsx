407da20c7984276a9d1c534cfe6e7eb000f4c40b
hsiangcoco <f1346790@gmail.com>
2024-12-05 12:00:45 +0800

    1205home更新

--- rcReact/src/pages/Home.jsx
+++ rcReact/src/pages/Home.jsx
@@ -3,33 +3,42 @@
 function Home() {
     return (
         <>
-            <div>
-                <p>用<span>程式</span>創造未來</p>
-            </div>
-            <p>Create the Future with Programming</p>
-            <div>
-                <p>用<span>英語</span>世界接軌</p>
-            </div>
-            <p>Connect the World in English</p>
-
-            <div>
-                <p>程式語言納入108課綱</p>
-                <p>Programming included in 108 syllabus</p>
-                <p>為了因應科技時代的需求，
-                    108課綱將「程式設計」正式納入中小學的資訊教育。
-                    通過基礎程式學習來鍛鍊孩子的邏輯思考、創意思維和問題解決能力，
-                    並且學會技術應用，強化數位素養。</p>
-            </div>
-            <div>
-                <p>2030雙語政策</p>
-                <p>Bilingual 2030</p>
-                <p>教育部正在推動「2030雙語政策」。
-                    目標是到2030年在國民教育中達成雙語環境，
-                    希望孩子不僅能在課堂上學習語法與詞彙，
-                    更能在日常生活和學習中靈活運用英語，
-                    讓孩子未來更具國際競爭力！</p>
-            </div>
-
+            <section id="page-2">{/* 第二分頁 */}
+                <div className="content-bk-1">
+                    <div className="content-1">
+                        <p className="text-1">用<span>程式</span>創造未來</p>
+                        <p className="eng-text-1">Create the Future with Programming</p>
+                    </div>
+                    <div className="content-2">
+                        <p className="text-1">用<span>英語</span>世界接軌</p>
+                        <p className="eng-text-2">Connect the World in English</p>
+                    </div>
+                </div>
+                <div className="content-bk-2">
+                    <div className="content-3">
+                        <p className="text-2">程式語言納入108課綱</p>
+                        <p className="eng-text-2">Programming included in 108 syllabus</p>
+                        <p className="sml-text-2">為了因應科技時代的需求，
+                            108課綱將「程式設計」正式納入中小學的資訊教育。
+                            通過基礎程式學習來鍛鍊孩子的邏輯思考、創意思維和問題解決能力，
+                            並且學會技術應用，強化數位素養。</p>
+                    </div>
+                    <div className="content-4">
+                        <p className="text-2">2030雙語政策</p>
+                        <p className="eng-text-2">Bilingual 2030</p>
+                        <p className="sml-text-2">教育部正在推動「2030雙語政策」。
+                            <br />目標是到2030年在國民教育中達成雙語環境，
+                            希望孩子不僅能在課堂上學習語法與詞彙，
+                            <br />更能在日常生活和學習中靈活運用英語，
+                            <br />讓孩子未來更具國際競爭力！</p>
+                    </div>
+                </div>
+                <figure id="homeDecoA"><img src="../public/images/homeDecoA.png" alt="" /></figure>
+                <figure id="blueA"><img src="../public/images/blueA.svg" alt="" /></figure>
+                <figure id="redIP"><img src="../public/images/red.svg" alt="" /></figure>
+                <figure id="homeDecoB"><img src="../public/images/homeDecoB.png" alt="" /></figure>
+                {/* <figure id="homePathA"><img src="../public/images/homePathA.png" alt="" /></figure> */}
+            </section>
             <div>
                 <div>
                     <p>課程 <span>枯燥乏味</span></p>
