08fb9d4faed83a1ca85d2a6272ebf88a99ea0815
hsiangcoco <f1346790@gmail.com>
2024-12-08 21:47:16 +0800

    1208 0946 首頁完成(差大圖）

--- rcReact/src/App.jsx
+++ rcReact/src/App.jsx
@@ -1,14 +1,18 @@
-import '../css/Home.css';
+
 import Footer from './components/footer'
+import Navbar from './components/navbar';
 import TrendCard from './components/TrendCard';
-import Home from './pages/Home'
+
 import LearnSelect from './pages/LearnSelect'
 import Trend from './pages/Trend';
+import '../css/Navbar.css';
+import '../css/Home.css';
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
-    <Home/>
+
     </>
   )
 }
---
+++ rcReact/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+    return (
+        <>
+            <section id="page-1">
+                <div className="navBarBk">
+                    <div className="logo">
+                        {/* <Link to=''>1234</Link> Link 打了之後出不來 */}
+                        <img src="../public/images/logo.png" alt="" />
+                    </div>
+                    <div className="navBarBk-1">
+                        <div className="learningBtn">
+                            <img src="../public/images/navBtn.png" alt="" />
+                        </div>
+                        <div className="hamBtn">
+                            <img src="../public/images/hamBtn.svg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+export default Navbar
