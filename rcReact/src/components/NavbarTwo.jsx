8ec27bc302a0b9c01dbe84454080d8721cfdcb8c
hsiangcoco <f1346790@gmail.com>
2024-12-09 18:01:05 +0800

    1209home更新

---
+++ rcReact/src/components/NavbarTwo.jsx
@@ -0,0 +1,27 @@
+
+
+
+function NavbarTwo() {
+    return (
+        <>
+            <section id="page-1">
+                <div className="navBarBk">
+                    <div className="logo">
+                        
+                        {/* <Link to=''>1234</Link> Link 打了之後出不來 */}
+
+                    </div>
+                    <div className="navBarBk-1">
+                        {/* <div className="learningBtn">
+                            <img src="../public/images/navBtn.png" alt="" />
+                        </div> */}
+                        <div className="hamBtn">
+                            <img src="../public/images/hamBtn.svg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+export default NavbarTwo
