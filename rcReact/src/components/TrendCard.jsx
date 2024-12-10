e63c4d0eeb4fa8dc7adb9660fcfcb179fa09a292
yeeeeefang <sweetcat881314@gmail.com>
2024-12-10 14:46:09 +0800

    trend1210

--- rcReact/src/components/TrendCard.jsx
+++ rcReact/src/components/TrendCard.jsx
@@ -2,13 +2,33 @@ import '../../css/TrendCard.css'
 function TrendCard() {
 
     return (
-        <div className="card">
-            <h3 className="learn">HTML</h3>
-            <img src="../public/images/trendCourse1.png" alt="" />
-            <div class="item">
-                <h4 className="learn">什麼是HTML?</h4>
-                <p className="content">學習人數 473人</p>
-                <p className="content">收藏人數 294人</p>
+        <div className="cardcontent">
+            <div className="card1">
+                <h3 className="learn">CSS</h3>
+                <img src="../public/images/trendCourse2.png" alt="" />
+                <div class="item">
+                    <h4 className="learn">什麼是CSS?</h4>
+                    <p className="content">學習人數 2391人</p>
+                    <p className="content">收藏人數 1022人</p>
+                </div>
+            </div>
+            <div className="card2">
+                <h3 className="learn">HTML</h3>
+                <img src="../public/images/trendCourse1.png" alt="" />
+                <div class="item">
+                    <h4 className="learn">什麼是HTML?</h4>
+                    <p className="content">學習人數 473人</p>
+                    <p className="content">收藏人數 294人</p>
+                </div>
+            </div>
+            <div className="card3">
+                <h3 className="learn">JS</h3>
+                <img src="../public/images/trendCourse3.png" alt="" />
+                <div class="item">
+                    <h4 className="learn">什麼是JS?</h4>
+                    <p className="content">學習人數 930人</p>
+                    <p className="content">收藏人數 467人</p>
+                </div>
             </div>
         </div>
     )
--- rcReact/src/pages/Trend.jsx
+++ rcReact/src/pages/Trend.jsx
@@ -1,12 +1,29 @@
 import TrendCard from "../components/TrendCard";
-
+import "../../css/Trend.css"
 
 function Trend(){
 
-    return(
-        <>
-        <TrendCard/>
-        </>
-    )
+    return (
+        <div id="div1" >
+          <div id="header"></div>
+          <div className="lightcontainer">
+            <img className="light" src="./public/images/trendLight.png" alt="" />
+            <div className="container">
+              <img className="light1" src="./public/images/trendLightA.svg" alt="" />
+              <img className="light2" src="./public/images/trendLightB.svg" alt="" />
+              <img className="light3" src="./public/images/trendLightC.svg" alt="" />
+            </div>
+          </div>
+          <TrendCard/>
+          <div id="item3">
+          <div className="item4"><img className="char-1" src="./public/images/blueA.svg" alt="" /></div>
+          <div className="item4"><img className="char-2" src="./public/images/red.svg" alt="" /></div>
+          <div className="item4"><img className="char-3" src="./public/images/pinkC.svg" alt="" /></div>
+          </div>
+          <div>
+            <img className="path" src="./public/images/trendPath.svg" alt="" />
+          </div>
+        </div>
+      )
 }
 export default Trend
