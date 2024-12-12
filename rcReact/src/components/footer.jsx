3d624c0593e01b8969f5819ec7cddc51e4c082d8
yeeeeefang <sweetcat881314@gmail.com>
2024-12-12 23:50:26 +0800

    1213

--- rcReact/src/components/footer.jsx
+++ rcReact/src/components/footer.jsx
@@ -6,17 +6,20 @@ function Footer() {
         <div>
             <nav className="nav1">
                 <div id="nav1-1">
-                    <ul className="item1"><p>首頁</p>
+                    <ul className="item1">
+                        <p className='pp1'>首頁</p>
                         <li className="item2"><a href=""><p>關於我們</p></a></li>
 
                     </ul>
-                    <ul className="item1"><p>學習專區</p>
+                    <ul className="item1">
+                        <p className='pp1'>學習專區</p>
                         <li className="item2"><a href=""><p>學習區</p></a></li>
                         <li className="item2"><a href=""><p>小試身手</p></a></li>
                         <li className="item2"><a href=""><p>探索學習趨勢</p></a></li>
 
                     </ul>
-                    <ul className="item1"><p>會員專區</p>
+                    <ul className="item1">
+                        <p className='pp1'>會員專區</p>
                         <li className="item2"><a href=""><p>登入/註冊</p></a></li>
                         <li className="item2"><a href=""><p>我的帳號</p></a></li>
 
