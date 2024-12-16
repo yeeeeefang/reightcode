51c1dd018a3968b46f0482d744fdd2127f773edc
yeeeeefang <sweetcat881314@gmail.com>
2024-12-16 09:10:52 +0800

    1216updatevite

--- rcReact/vite.config.js
+++ rcReact/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:process.env.NODE_ENV==='production'?'/rcReact/':'/',
+  // base:process.env.NODE_ENV==='production'?'/rcReact/':'/',
+  base:'./',
   plugins: [react()],
   
 })
