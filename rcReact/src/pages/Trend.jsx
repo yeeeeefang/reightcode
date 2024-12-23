b993e107f7f86da8b7ce2eeabd691b3dd47459d9
yeeeeefang <sweetcat881314@gmail.com>
2024-12-23 13:03:06 +0800

    banner&trend

--- rcReact/src/pages/Trend.jsx
+++ rcReact/src/pages/Trend.jsx
@@ -3,16 +3,39 @@ import TrendCard2 from "../components/TrendCard2";
 import "../assets/scss/Trend.scss"
 import Navbar from '../components/Navbar'
 import Footer from "../components/footer";
+import { useEffect } from "react";
 
-function Trend(){
+export default function Trend() {
+    useEffect(() => {
+      const handleScroll = () => {
+        const div2 = document.querySelector('#div2');
+        const threshold = 100; // 滾動高度門檻
+
+        if (div2) {
+          const rect = div2.getBoundingClientRect(); // 獲取元素相對於視窗的位置
+          if (rect.top < window.innerHeight && rect.bottom > 0) {
+            div2.classList.add('scrolled'); // 滾動進入視窗範圍時，添加類名
+          } else {
+            div2.classList.remove('scrolled'); // 滾動離開視窗時，移除類名
+          }
+        }
+      };
+
+      window.addEventListener('scroll', handleScroll);
+
+      // 清除滾動事件監聽
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
 
     return (
       <>
         <section id="div1" >
-        <Navbar/>
-                   
+          <Navbar />
+
           <div className="lightcontainer">
-          
+
             <img className="light" src="./public/images/trendLight.png" alt="" />
             <h3 className="hottitle">熱門學習趨勢</h3>
             <div className="container">
@@ -21,26 +44,26 @@ function Trend(){
               <img className="light3" src="./public/images/trendLightC.svg" alt="" />
             </div>
           </div>
-          <TrendCard/>
+          <TrendCard />
           <img className="deco-1" src="./public/images/trendDecoA.png" alt="" />
           <img className="deco-2" src="./public/images/trendDecoB.png" alt="" />
           <img className="deco-3" src="./public/images/trendDecoC.png" alt="" />
           <div id="item3">
-          <div className="item4"><img className="char-1" src="./public/images/blueA.svg" alt="" /></div>
-          <div className="item4"><img className="char-2" src="./public/images/redC.svg" alt="" /></div>
-          <div className="item4"><img className="char-3" src="./public/images/pinkE.svg" alt="" /></div>
+            <div className="item4"><img className="char-1" src="./public/images/blueA.svg" alt="" /></div>
+            <div className="item4"><img className="char-2" src="./public/images/redC.svg" alt="" /></div>
+            <div className="item4"><img className="char-3" src="./public/images/pinkE.svg" alt="" /></div>
           </div>
           <div >
             <img className="path" src="./public/images/trendPath.svg" alt="" />
           </div>
         </section>
         <section id="div2">
-          <TrendCard2/>
+          <TrendCard2 />
         </section>
         <section>
-          <Footer/>
+          <Footer />
         </section>
-        </>
-      )
-}
-export default Trend
+      </>
+    )
+  }
+
