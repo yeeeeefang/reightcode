3a7ee39931983cc731a8e8637f6459cdc8a274ce
TYCHIEN1120 <tychien1120@outlook.com>
2024-12-28 00:55:12 +0800

    我彈不出來T.T

--- rcReact/src/pages/Login.jsx
+++ rcReact/src/pages/Login.jsx
@@ -1,22 +1,21 @@
+
+import { useState } from "react";
 import "../assets/scss/Login.scss"
 import Navbar from "../components/Navbar"
 
 
+
 export default function Login() {
+
+    const [forgot, setfFrgot] = useState(false);
+    const forgotOpen = () => { setfFrgot(true); }
+    const forgotClose = () => { setfFrgot(false); }
+
     return (
         <>
-        
-            <div id="login-page">  
-            <Navbar/>
-                {/* topbar  */}
-                {/* <header className="topbar">
-                    <div className="logo">
-                        <h1 className="logo"><img src="/images/logo.png" alt="rightCode" title="rightCode" /></h1>
-                    </div>
-                    <div className="learnBtn">
-                        <figure><img src="/images/hamBtn.svg" alt="" /></figure>
-                    </div>
-                </header> */}
+
+            <div id="login-page">
+                <Navbar />
                 <div className="login-main">
                     <div className="amoeba-blobs">
                         <img src="/images/login.png" alt="" />
@@ -24,7 +23,7 @@ export default function Login() {
 
                     <div className="characters">
                         <img src="/images/greenA.svg" alt="" className="green-char" />
-                        <img src="/images/redB.svg" alt="" className="red-char" /> 
+                        <img src="/images/redB.svg" alt="" className="red-char" />
                         <img src="/images/blueA.svg" alt="" className="blue-char" />
                         <img src="/images/pinkA.svg" alt="" className="pink-char" />
                     </div>
@@ -33,12 +32,12 @@ export default function Login() {
                         <h2>會員登入</h2>
                         <form>
                             <label htmlFor="email">*Email：</label>
-                            <input type="email" name="email" id="email" title="Email" placeholder="請輸入Email" required autoFocus/>
+                            <input type="email" name="email" id="email" title="Email" placeholder="請輸入Email" required autoFocus />
 
                             <label htmlFor="password">*請輸入密碼：</label>
                             <input type="password" name="password" id="password" title="重設密碼" placeholder="請輸入6~8英數混合字元" />
 
-                            <a href="#" className="forgot-password">忘記密碼</a>
+                            <p className="forgot-password" onClick={forgotOpen}>忘記密碼</p>
 
                             <button type="submit" className="login-button">登入</button>
                         </form>
@@ -54,7 +53,22 @@ export default function Login() {
                         </div>
                     </div>
                 </div>
+                {forgot && (
+                    <div className="forgot" onClick={forgotClose}>
+                        <div className="forgot-form">
+                            <label htmlFor="email">*Email：</label>
+                            <input type="email" name="email" id="email" title="Email" placeholder="請輸入Email" required />
+                            <label htmlFor="password">*請輸入密碼：</label>
+                            <input type="password" name="password" id="password" title="密碼" placeholder="請輸入6~8英數混合字元" />
+                            <div className="remember-password">
+                                <input type="checkbox" name="remember" id="remember" title="記住密碼" value="記住密碼" />
+                                <label htmlFor="">記住這個帳號密碼</label>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
-        </> 
+
+        </>
     )
 }
