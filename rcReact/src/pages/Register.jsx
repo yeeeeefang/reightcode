78a19879293841eed0812f7e618cbf5a5324c192
TYCHIEN1120 <tychien1120@outlook.com>
2024-12-27 17:32:18 +0800

    定位成功

--- rcReact/src/pages/Register.jsx
+++ rcReact/src/pages/Register.jsx
@@ -32,18 +32,21 @@ export default function Register() {
                     <div className="register-form">
                         <h2>會員註冊</h2>
                         <form>
-                            <label htmlFor="username">*姓名：</label>
-                            <input type="text" name="username" id="username" title="姓名" placeholder="請輸入姓名" required autofocus />
-                            <label htmlFor="email">*Email：</label>
-                            <input type="email" name="email" id="email" title="Email" placeholder="請輸入Email" required />
+                            <div className="register-form-content">
+                                <label htmlFor="username">*姓名：</label>
+                                <input type="text" name="username" id="username" title="姓名" placeholder="請輸入姓名" required autofocus />
+                                <label htmlFor="email">*Email：</label>
+                                <input type="email" name="email" id="email" title="Email" placeholder="請輸入Email" required />
 
-                            <label htmlFor="password">*請輸入密碼：</label>
-                            <input type="password" name="password" id="password" title="密碼" placeholder="請輸入6~8英數混合字元" />
-                            {/* <label  htmlFor="password-1">再次確認密碼</label>
+                                <label htmlFor="password">*請輸入密碼：</label>
+                                <input type="password" name="password" id="password" title="密碼" placeholder="請輸入6~8英數混合字元" />
+                                {/* <label  htmlFor="password-1">再次確認密碼</label>
                             <input type="password" name="password" id="password-1" title="再次確認密碼" placeholder="請輸入6~8英數混合字元" /> */}
-
-                            <a href="#" className="forgot-password">記住這個帳號密碼</a>
-
+                            </div>
+                            <div className="remember-password">
+                                <input type="checkbox" name="remember" id="remember" title="記住密碼" value="記住密碼" />
+                                <label htmlFor="">記住這個帳號密碼</label>
+                            </div>
                             <button type="submit" className="register-button">登入</button>
                         </form>
                         <div className="register-divider">
